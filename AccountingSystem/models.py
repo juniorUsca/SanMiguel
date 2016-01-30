ede21ca5be7e6221573a2bbd1170c918a0edf902
junior usca <junior.usca@gmail.com>
2016-01-30 16:06:37 +0000

    added models, configuring admin

--- AccountingSystem/models.py
+++ AccountingSystem/models.py
@@ -1,5 +1,55 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from datetime import date, datetime
 
-# Create your models here.
+@python_2_unicode_compatible
+class Category(models.Model):
+    DEFAULT_PK = 1
+    
+    name = models.CharField(max_length=50)
+    
+    created = models.DateTimeField(null=True, blank=True)
+    updated = models.DateTimeField(null=True, blank=True)
+    #created_by = models.ForeignKey(User)
+    
+    def save(self):
+        if not self.id:
+            self.created = datetime.today()
+        self.updated = datetime.today()
+        super(Category, self).save()
+    
+    def __str__(self):
+        return self.name
+    
+@python_2_unicode_compatible
+class AccountingSystem(models.Model):
+    INCOME = 'IN'
+    EXPENSE = 'EX'
+    TYPES_CHOICES = (
+        (INCOME, 'Income'),
+        (EXPENSE, 'Expense'),
+    )
+    
+    type = models.CharField( max_length = 2,
+                             choices = TYPES_CHOICES,
+                             default = INCOME )
+    amount = models.FloatField()
+    category = models.ForeignKey( Category,
+                                  default = Category.DEFAULT_PK,
+                                  on_delete = models.SET_DEFAULT )
+    date = models.DateTimeField('date published')
+    note = models.CharField(max_length=200)
+    
+    created = models.DateTimeField(null=True, blank=True)
+    updated = models.DateTimeField(null=True, blank=True)
+    #created_by = models.ForeignKey(User)
+    
+    def save(self):
+        if not self.id:
+            self.created = datetime.today()
+        self.updated = datetime.today()
+        super(AccountingSystem, self).save()
+
+    def __str__(self):
+        return self.amount
--- sanmiguel/settings.py
+++ sanmiguel/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AccountingSystem.apps.AccountingsystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
 TIME_ZONE = 'UTC'
 
